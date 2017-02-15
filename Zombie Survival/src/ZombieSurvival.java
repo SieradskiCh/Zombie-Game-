be82b57445a58074a256232cdb65afcc706014ae
chase.sieradski <chase.sieradski@MU-C1-07.mullen.pvt.k12.co.us>
2017-02-15 14:58:28 -0700

    second addition

--- Zombie Survival/src/ZombieSurvival.java
+++ Zombie Survival/src/ZombieSurvival.java
@@ -8,7 +8,13 @@ public class ZombieSurvival extends JFrame
 	private static final long serialVersionUID = 1L;
 	static String name; 
 	static int className;
-	static int seaStormCounter;
+	static int zombieBeat = 0;
+	static int bearBeat = 0;
+	static int wildNight = 0;
+	static int wildStick = 0;
+	static int seaStormCounter = 0;
+	static int fishCaught = 0;
+	static int sharkSurvived = 0;
 	static JFrame frame = new JFrame();
 
 	public static void main(String[] args)
@@ -166,6 +172,65 @@ public class ZombieSurvival extends JFrame
 			survivorWildAttack();
 			}
 		}
+	public static void survivorWildShelter()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "That looks like a good place to spend the night! Go inside?",
+				"Shelter",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 0)
+			{
+			wildNight = wildNight + 1;
+			JOptionPane.showMessageDialog(frame, "Guess you're not completely stupid. You've survived " + wildNight + " nights.");
+			survivorWild();
+			}
+		else if (userChoice == 1)
+			{
+			JOptionPane.showMessageDialog(frame, "........why?");
+			survivorWild();
+			}
+		}
+	public static void survivorWildZombie()
+		{
+		JOptionPane.showMessageDialog(frame, "A zombie?! In the middle of nowhere???");
+		int attack = (int)(Math.random()* 100);
+		if (wildStick > 0)
+			{
+			if (attack >= 0 && attack < 61)
+				{
+				zombieBeat = zombieBeat + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the zombie with your will and your sharp stick. That's " + zombieBeat + " attacks survived.");
+				survivorWild();
+				}
+			else if (attack >= 61 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've beaten " + zombieBeat + " zombies, " + bearBeat + " bears, and you made " + wildStick + " pointy sticks.");
+				survivorDeath();
+				}
+			}
+		else 
+			{
+			if (attack >= 0 && attack < 41)
+				{
+				zombieBeat = zombieBeat + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the zombie with your will and your sharp stick. That's " + zombieBeat + " attacks survived.");
+				survivorWild();
+				}
+			else if (attack >= 41 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've beaten " + zombieBeat + " zombies, " + bearBeat + " bears, and you made " + wildStick + " pointy sticks.");
+				survivorDeath();
+				}	
+			}
+		}
+	public static void survivorWildWeapons()
+		{
+		wildStick = wildStick + 1;
+		JOptionPane.showMessageDialog(frame, "You had some time on your hands, so you made a weapon. You've crafted " + wildStick + " sharp sticks.");
+		survivorWild();
+		}
 	public static void survivorSea()
 		{
 		int path = (int)(Math.random()* 100);
@@ -186,6 +251,62 @@ public class ZombieSurvival extends JFrame
 			survivorIslandEnd();
 			}
 		}
+	public static void survivorSeaSharkAttack()
+		{
+		JOptionPane.showMessageDialog(frame, "It's like 3-D Jaws!");
+		int attack = (int)(Math.random()* 100);
+		if (fishCaught > 4)
+			{
+			if (attack >= 0 && attack < 61)
+				{
+				sharkSurvived = sharkSurvived + 1;
+				fishCaught = fishCaught + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the shark with your bare hands, fueled by fish protien. That's " + sharkSurvived + " attacks survived and " + fishCaught + " fish caught.");
+				survivorSea();
+				}
+			else if (attack >= 61 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've survived " + seaStormCounter + " storms, " + sharkSurvived + " shark attacks and you caught " + fishCaught + " fish.");
+				survivorDeath();
+				}
+			}
+		else 
+			{
+			if (attack >= 0 && attack < 41)
+				{
+				sharkSurvived = sharkSurvived + 1;
+				fishCaught = fishCaught + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the shark with your bare hands, fueled by fish protien. That's " + sharkSurvived + " attacks survived and " + fishCaught + " fish caught.");
+				survivorSea();
+				}
+			else if (attack >= 41 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've survived " + seaStormCounter + " storms, " + sharkSurvived + " shark attacks and you caught " + fishCaught + " fish.");
+				survivorDeath();
+				}
+			}
+		}
+	public static void survivorSeaFishing()
+		{
+		JOptionPane.showMessageDialog(frame, "Hours on end holding a pole? Sounds fun!");
+		int fish = (int)(Math.random()* 100);
+		if (fish >= 0 && fish < 33)
+			{
+			fishCaught = fishCaught + 1;
+			JOptionPane.showMessageDialog(frame, "You lucky son of a gun...looks like you reeled one in. That's a total of " + fishCaught + " figh caught.");
+			survivorSea();
+			}
+		else if (fish >= 33 && fish < 66)
+			{
+			JOptionPane.showMessageDialog(frame, "Wait, hold up...it looks like them clouds are gettin a bit darker");
+			survivorSeaStorm();
+			}
+		else if (fish >= 66 && fish <= 100)
+			{
+			JOptionPane.showMessageDialog(frame, "Is...is that water red?!");
+			survivorSeaSharkAttack();
+			}
+		}
 	public static void survivorSeaStorm()
 		{
 		JOptionPane.showMessageDialog(frame, "Get your poncho on and your barf bucket ready! I see a storm on the horizon!");
@@ -196,7 +317,7 @@ public class ZombieSurvival extends JFrame
 			JOptionPane.showMessageDialog(frame, "You lucky son of a gun...looks like you lived through this one. That's " + seaStormCounter + " storms survived.");
 			survivorSea();
 			}
-		else if (storm >= 51 && storm < 100)
+		else if (storm >= 51 && storm <= 100)
 			{
 			JOptionPane.showMessageDialog(frame, "Lol. Killed by nature. That hasen't happened in like forever.");
 			}
@@ -222,7 +343,7 @@ public class ZombieSurvival extends JFrame
 	public static void survivorDeath()
 		{
 		Object[] options = {"Yes", "No"};
-		int userChoice = JOptionPane.showOptionDialog(frame, "You had a good run, but for whatever reason, you gave in and died like a little bitch. Now, would you like to continue playing as a zombie?",
+		int userChoice = JOptionPane.showOptionDialog(frame, "Would you like to continue playing as a zombie?",
 				"RIP",
 				JOptionPane.YES_NO_CANCEL_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
