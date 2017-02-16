98bdb6de967f7e30a5a651ed3d921b8fdf7a1a7e
Chase.Sieradski <Chase.Sieradski@MU-C1-07.mullen.pvt.k12.co.us>
2017-02-16 14:48:55 -0700

    final commit-game is running

--- Zombie Survival/src/ZombieSurvival.java
+++ Zombie Survival/src/ZombieSurvival.java
@@ -6,7 +6,9 @@ import javax.swing.*;
 public class ZombieSurvival extends JFrame
 	{
 	private static final long serialVersionUID = 1L;
-	static String name; 
+	static String name;
+	static int humansKilled = 0;
+	static int zombieBearBeat = 0;
 	static int className;
 	static int gun = 0;
 	static int zombieBeat = 0;
@@ -29,8 +31,11 @@ public class ZombieSurvival extends JFrame
 				JOptionPane.YES_NO_CANCEL_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
 				null, options, options[2]);
-
-		switch(className)
+		pathAssign();
+	    }
+	public static void pathAssign()
+		{
+			switch(className)
 			{
 			case 0:
 				{
@@ -84,7 +89,7 @@ public class ZombieSurvival extends JFrame
 			}
 		else if (className == 1 || className == 2)
 			{
-			final String[] genre = { "Big City", "Small Town", "Wilderness", "Beach" };
+			final String[] genre = { "Big City", "Small Town", "Wilderness", };
 				{
 				final JFrame frame = new JFrame();
 				String adventure = (String) JOptionPane.showInputDialog(frame, 
@@ -97,18 +102,17 @@ public class ZombieSurvival extends JFrame
 				if (adventure.equals("Big City"))
 					{
 					JOptionPane.showMessageDialog(frame, "A social zombie eh?");
+					zombieCity();
 					}
 				else if (adventure.equals("Small Town"))
 					{
 					JOptionPane.showMessageDialog(frame, "Ah that small town life. Good people, good views, and really really good brains!");
+					zombieTown();
 					}
 				else if (adventure.equals("Wilderness"))
 					{
 					JOptionPane.showMessageDialog(frame, "Because you're a zombie. You do what you want.");
-					}
-				else if (adventure.equals("Ocean"))
-					{
-					JOptionPane.showMessageDialog(frame, "Hey, the undead needs sick tans too dawg!");
+					zombieWild();
 					}
 				}
 			}
@@ -131,6 +135,137 @@ public class ZombieSurvival extends JFrame
 		else if (path >= 60 && path <= 100)
 			{
 			survivorCityZombie();
+			}
+		}
+	public static void survivorCityShelter()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "That looks like a good place to spend the night! Go inside?",
+				"Shelter",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 0)
+			{
+			wildNight = wildNight + 1;
+			if (zombieBeat > 5)
+				{
+				int chance = (int)(Math.random()* 100);
+				if (chance > 97 && chance <= 100)
+					{
+					JOptionPane.showMessageDialog(frame, "Hmm...it looks like this shelter will last a while. Guess you beat the game! You survived " + wildNight + " nights and beat " + zombieBeat + " zombies. You also found " + gun + " guns.");
+					System.exit(0);
+					}
+				else 
+					{
+					JOptionPane.showMessageDialog(frame, "Guess you're not completely stupid. You've survived " + wildNight + " nights.");
+					survivorCity();
+					}
+				}
+			else 
+				{
+				JOptionPane.showMessageDialog(frame, "Guess you're not completely stupid. You've survived " + wildNight + " nights.");
+				survivorCity();
+				}
+			}
+		else if (userChoice == 1)
+			{
+			JOptionPane.showMessageDialog(frame, "........why?");
+			survivorCity();
+			}
+		}
+	public static void survivorCityFood()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "An old grocery store. Go inside?",
+				"Wow",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 0)
+			{
+			int choice = (int)(Math.random()* 100);	
+			if (choice >= 0 && choice <=50)
+				{
+				JOptionPane.showMessageDialog(frame, "Oh lordie! There be zombies in here!");
+				survivorCityZombie();
+				}
+			else 
+				{
+				JOptionPane.showMessageDialog(frame, "Looks like there's nothing here...");
+				survivorCity();
+				}
+			}
+		else 
+			{
+			JOptionPane.showMessageDialog(frame, "Movin on!");
+			survivorCity();
+			}
+		}
+	public static void survivorCityWeapons()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "An old sporting goods store. Go inside?",
+				"Chance for Weapon",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 0)
+			{
+			int choice = (int)(Math.random()* 100);	
+			if (choice >= 0 && choice <= 20)
+				{
+				JOptionPane.showMessageDialog(frame, "Oh lordie! There be zombies in here!");
+				survivorCityZombie();
+				}
+			else if (choice > 20 && choice <= 40)
+				{
+				gun = gun + 1;
+				JOptionPane.showMessageDialog(frame, "A gun! That'll help calm some zombies!");
+				}
+			else 
+				{
+				JOptionPane.showMessageDialog(frame, "Looks like there's nothing here...");
+				survivorCity();
+				}
+			}
+		else 
+			{
+			JOptionPane.showMessageDialog(frame, "Hey man it's your life. Your short and miserable life...");
+			survivorCity();
+			}
+		}
+	public static void survivorCityZombie()
+		{
+		JOptionPane.showMessageDialog(frame, "A zombie! Fight for your life man!!!");
+		int attack = (int)(Math.random()* 100);
+		if (gun > 0)
+			{
+			if (attack >= 0 && attack < 81)
+				{
+				zombieBeat = zombieBeat + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the zombie with your will and your .45. That's " + zombieBeat + " attacks survived.");
+				survivorCity();
+				}
+			else if (attack >= 81 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've beaten " + zombieBeat + " zombies, found " + gun + " guns, and survived " + wildNight + " nights.");
+				survivorDeath();
+				}
+			}
+		else 
+			{
+			if (attack >= 0 && attack < 41)
+				{
+				zombieBeat = zombieBeat + 1;
+				JOptionPane.showMessageDialog(frame, "In an epic battle, you defeat the zombie with your will and your bare hands. That's " + zombieBeat + " attacks survived.");
+				survivorCity();
+				}
+			else if (attack >= 41 && attack <= 100)
+				{
+				JOptionPane.showMessageDialog(frame, "You died. You've beaten " + zombieBeat + " zombies, found " + gun + " guns, and survived " + wildNight + " nights.");
+				survivorDeath();
+				}	
 			}
 		}
 	public static void survivorTown()
@@ -517,7 +652,7 @@ public class ZombieSurvival extends JFrame
 			JOptionPane.showMessageDialog(frame, "You idiot! Do you know the odds of finding another island?!");
 			survivorSea();	
 			}
-		else if (userChoice == 1)
+		else if (userChoice == 0)
 			{
 			JOptionPane.showMessageDialog(frame, "You're feet touch land. Land free of zombies and filled with food. I have to say I'm impressed. You've somehow survived the apocalypse.");
 			}
@@ -532,11 +667,248 @@ public class ZombieSurvival extends JFrame
 				null, options, options[1]);
 		if (userChoice == 0)
 			{
-			className = 1;	
+			className = 1;
+			pathAssign();
 			}
 		else if (userChoice == 1)
 			{
 			JOptionPane.showMessageDialog(frame, "Thanks for playing! Come again soon.");
+			}
+		}
+	public static void zombieCity()
+		{
+		int path = (int)(Math.random()* 100);
+		if (path >= 0 && path < 71)
+			{
+			zombieCityHuman();
+			}
+		else if (path >= 71 && path < 100)
+			{
+			zombieCityWander();
+			}
+		}
+	public static void zombieCityHuman()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "A human! You feeling hungry??",
+				"In the distance...",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 1)
+			{
+			JOptionPane.showMessageDialog(frame, "Fine don't then...loser");
+			zombieCity();	
+			}
+		else if (userChoice == 0)
+			{
+			int attack = (int)(Math.random()* 100);
+			if (humansKilled > 9)
+				{
+				if (attack >= 0 && attack < 80)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieCity();
+					}
+				else 
+					{
+					zombieDeath();
+					}
+				}
+			else 
+				{
+				if (attack >= 0 && attack < 60)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieCity();
+					}
+				else 
+					{
+					zombieDeath();
+					}
+				}
+			}
+		}
+	public static void zombieCityWander()
+		{
+		JOptionPane.showMessageDialog(frame, "You know, most of a zombie's job is just waiting around for a human to show up...");
+		zombieCity();
+		}
+	public static void zombieTown()
+		{
+		int path = (int)(Math.random()* 100);
+		if (path >= 0 && path < 61)
+			{
+			zombieTownHuman();
+			}
+		else if (path >= 61 && path < 100)
+			{
+			zombieTownWander();
+			}
+		}
+	public static void zombieTownHuman()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "A human! You feeling hungry??",
+				"In the distance...",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 1)
+			{
+			JOptionPane.showMessageDialog(frame, "Fine don't then...loser");
+			zombieTown();	
+			}
+		else if (userChoice == 0)
+			{
+			int attack = (int)(Math.random()* 100);
+			if (humansKilled > 9)
+				{
+				if (attack >= 0 && attack < 80)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieTown();
+					}
+				else 
+					{
+					zombieDeath();
+					}
+				}
+			else 
+				{
+				if (attack >= 0 && attack < 60)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieTown();
+					}
+				else 
+					{
+					zombieDeath();
+					}
+				}
+			}
+		}
+	public static void zombieTownWander()
+		{
+		JOptionPane.showMessageDialog(frame, "You know, most of a zombie's job is just waiting around for a human to show up...");
+		zombieTown();
+		}
+	public static void zombieWild()
+		{
+		int path = (int)(Math.random()* 100);
+		if (path >= 0 && path < 31)
+			{
+			zombieWildHuman();
+			}
+		else if (path >= 31 && path <61)
+			{
+			zombieWildBear();
+			}
+		else if (path >= 61 && path < 100)
+			{
+			zombieWildWander();
+			}
+		}
+	public static void zombieWildHuman()
+		{
+		Object[] options = {"Yes", "No"};
+		int userChoice = JOptionPane.showOptionDialog(frame, "A human! You feeling hungry??",
+				"In the distance...",
+				JOptionPane.YES_NO_CANCEL_OPTION,
+				JOptionPane.QUESTION_MESSAGE,
+				null, options, options[1]);
+		if (userChoice == 1)
+			{
+			JOptionPane.showMessageDialog(frame, "Fine don't then...loser");
+			zombieWild();	
+			}
+		else if (userChoice == 0)
+			{
+			int attack = (int)(Math.random()* 100);
+			if (humansKilled > 9)
+				{
+				if (attack >= 0 && attack < 80)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieWild();
+					}
+				else 
+					{
+					zombieDeath();
+					}
+				}
+			else
+				{
+				if (attack >= 0 && attack < 60)
+					{
+					humansKilled = humansKilled + 1;
+					JOptionPane.showMessageDialog(frame, "What a fight! You've now killed and eaten " + humansKilled + " humans.");
+					zombieWild();
+					}
+				else 
+					{
+					zombieDeath();
+					}	
+				}
+			}
+		}
+	public static void zombieWildBear()
+		{
+		JOptionPane.showMessageDialog(frame, "Oh God! A Bear!! Bears are not good for zombies!");
+		int attack = (int)(Math.random()* 100);
+		if (humansKilled > 9)
+			{
+			if (attack >= 0 && attack < 80)
+				{
+				zombieBearBeat = zombieBearBeat + 1;
+				JOptionPane.showMessageDialog(frame, "What a fight! You've now defeated " + zombieBearBeat + " bears.");
+				zombieWild();
+				}
+			else 
+				{
+				zombieDeath();
+				}
+			}
+		else
+			{
+			if (attack >= 0 && attack < 60)
+				{
+				zombieBearBeat = zombieBearBeat + 1;
+				JOptionPane.showMessageDialog(frame, "What a fight! You've now defeated " + zombieBearBeat + " bears.");
+				zombieWild();
+				}
+			else 
+				{
+				zombieDeath();
+				}	
+			}
+		}
+	public static void zombieWildWander()
+		{
+		JOptionPane.showMessageDialog(frame, "You know, most of a zombie's job is just waiting around for a human to show up...");
+		zombieWild();
+		}
+	public static void zombieDeath()
+		{
+		if (humansKilled < 10)
+			{
+			JOptionPane.showMessageDialog(frame, "You've been killed. It hurts more the second time around.");
+			JOptionPane.showMessageDialog(frame, "What a crappy zombie! You only managed to kill " + humansKilled + " humans...");
+			}
+		else if (humansKilled >= 10 && humansKilled < 20)
+			{
+			JOptionPane.showMessageDialog(frame, "You've been killed. It hurts more the second time around.");
+			JOptionPane.showMessageDialog(frame, "Looks like you're an average zombie! You managed to kill " + humansKilled + " humans.");
+			}
+		else
+			{
+			JOptionPane.showMessageDialog(frame, "You've been killed. It hurts more the second time around.");
+			JOptionPane.showMessageDialog(frame, "You, my friend, are a god among zombies! You killed " + humansKilled + " humans!");
 			}
 		}
 	}
